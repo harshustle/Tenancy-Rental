fecd8359059c42a981077e224eafb498d2b7c43f
Harsh Srivastav <112232698+harshustle@users.noreply.github.com>
2025-04-13 19:00:02 +0530

    Update user.js
    reverted the last two changes

--- routes/user.js
+++ routes/user.js
@@ -4,21 +4,8 @@ const User = require("../model/user.js");
 const passport = require("passport");
 const { saveRedirecturl } = require("../middleware.js");
 const userController = require("../controllers/users.js");
-const wrapAsync = require("../utils/wrapAsync.js");
-const { isLoggedIn, isOwner, validatelisting } = require("../middleware.js");
 
 // router.route
-router
-  .route("/")
-  .get(wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    upload.single("listing[image]"),
-    validatelisting,
-    wrapAsync(listingController.createListing)
-  );
-
-
 router
   .route("/signup")
   .get(userController.signupForm)
